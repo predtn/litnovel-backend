f75182bc46d003dfb8dd552be09c6a99d11d460c
Dungpt <phamtiendung508@gmail.com>
2026-06-29 09:16:55 +0700

    Fix path

--- LitNovel.Application/UseCases/Validators/User/UpdateMyProfileRequestValidator.cs
+++ LitNovel.Application/UseCases/Validators/User/UpdateMyProfileRequestValidator.cs
@@ -9,10 +9,19 @@ namespace LitNovel.Application.UseCases.Validators.User
         {
             RuleFor(x => x.Avatar)
                 .MaximumLength(512)
-                .Must(x => string.IsNullOrWhiteSpace(x) || Uri.TryCreate(x, UriKind.Absolute, out _))
-                .WithMessage("Avatar must be a valid URL");
+                .Must(BeValidAssetReference)
+                .WithMessage("Avatar must be a valid URL or local upload path");
 
             RuleFor(x => x.Bio).MaximumLength(1000);
+        }
+
+        private static bool BeValidAssetReference(string? value)
+        {
+            if (string.IsNullOrWhiteSpace(value)) return true;
+            if (value.StartsWith("/uploads/", StringComparison.OrdinalIgnoreCase)) return true;
+
+            return Uri.TryCreate(value, UriKind.Absolute, out var uri)
+                && (uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps);
         }
     }
 }
